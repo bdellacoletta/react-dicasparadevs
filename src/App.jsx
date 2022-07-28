b645097b507e62af076c353171ad1e79ee1c6cb8
Branca Scannapieco Della Coletta <brancasdc@gmail.com>
2022-07-28 11:48:45 -0300

    correção de bugs

--- src/App.jsx
+++ src/App.jsx
@@ -12,18 +12,7 @@ import './App.css';
 
 
 const App = () => {
-  const [tasks, setTasks] = useState([
-    {
-      id: '1',
-      title: 'Estudar programação',
-      completed: false,
-    },
-    {
-      id: '2',
-      title: 'Ler livros',
-      completed: true,
-    }
-  ]);
+  const [tasks, setTasks] = useState([]);
 
     useEffect(() => {
       const fetchTasks = async () => {
