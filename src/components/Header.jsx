04c9aa7c7d0b9d6ede2d0ef849b41d3b58032dcf
Branca Scannapieco Della Coletta <brancasdc@gmail.com>
2022-07-27 21:17:03 -0300

    adicionado icone de 'informação'

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 import './Header.css'
 
 const Header = () => {
-  return <h1 className='header-title'>Minhas Tarefas</h1>;
+  return <h1 className='header'>Minhas Tarefas</h1>;
 }
 
 export default Header;
